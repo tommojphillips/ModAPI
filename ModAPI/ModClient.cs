39ea18e1137b99214834272b89ad46c558bbe862
Tommo J. Phillips <tommojphillips@gmail.com>
2018-10-19 01:25:56 +1100

    Updated to version 0.1.1.2

--- ModAPI/ModClient.cs
+++ ModAPI/ModClient.cs
@@ -15,7 +15,7 @@ namespace ModApi
         /// <summary>
         /// Represents the version of the api.
         /// </summary>
-        public static string version = "0.1.1.2";// System.Reflection.Assembly.GetEntryAssembly().GetName().Version.ToString();
+        public static string version = "0.1.1.2";
         /// <summary>
         /// Represents the assemble assemble audio source.
         /// </summary>
